1c03694dfbda6dccd3748618db6bcaf0ea31546a
Melkory <carloseduardo.dziombra@hotmail.com>
2025-04-24 19:35:31 -0300

    Método obterUsuarioMatricula

--- src/main/java/sisrh/rest/UsuariosRest.java
+++ src/main/java/sisrh/rest/UsuariosRest.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import io.swagger.annotations.Api;
 import sisrh.banco.Banco;
@@ -23,6 +25,23 @@ public class UsuariosRest {
 		List<Usuario> lista = Banco.listarUsuarios();
 		GenericEntity<List<Usuario>> entity = new GenericEntity<List<Usuario>>(lista) {};
 		return Response.ok().entity(entity).build();
+	}
+	
+	@GET
+	@Path("{matricula}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response obterUsuarioMatricula (@PathParam("matricula") String matricula) throws Exception {
+		try {
+			Usuario usuario = Banco.buscarUsuarioPorMatricula(matricula);
+			
+			if (usuario != null) {
+				return Response.ok().entity(usuario).build();
+			} else {
+				return Response.status(Status.NOT_FOUND).entity("{ \"mensagem\" : \"Usuario nao encontrado!\" }").build();
+			}
+		} catch (Exception e) {
+				return Response.status(Status.INTERNAL_SERVER_ERROR).entity("{ \"mensagem\" : \"Falha para obter usuario!\" }").build();
+		}
 	}
 
 }
